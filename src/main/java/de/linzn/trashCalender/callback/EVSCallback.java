30c03bf52b335cc353d043d74ca6078ef3d20c65
Niklas <niklas.linz@enigmar.de>
2020-03-08 13:26:17 +0100

    update

--- src/main/java/de/linzn/trashCalender/callback/EVSCallback.java
+++ src/main/java/de/linzn/trashCalender/callback/EVSCallback.java
@@ -14,9 +14,8 @@ package de.linzn.trashCalender.callback;
 import de.azcore.azcoreRuntime.AppLogger;
 import de.azcore.azcoreRuntime.taskManagment.AbstractCallback;
 import de.azcore.azcoreRuntime.taskManagment.CallbackTime;
-import de.azcore.azcoreRuntime.taskManagment.operations.OperationRegister;
-import de.azcore.azcoreRuntime.taskManagment.operations.OperationSettings;
-import de.azcore.azcoreRuntime.taskManagment.operations.TaskOperation;
+import de.azcore.azcoreRuntime.taskManagment.operations.OperationOutput;
+import de.azcore.azcoreRuntime.taskManagment.operations.defaultOperations.ShellOperation;
 import de.azcore.azcoreRuntime.utils.Color;
 import de.linzn.trashCalender.TrashCalenderPlugin;
 
@@ -28,20 +27,18 @@ public class EVSCallback extends AbstractCallback {
     @Override
     public void operation() {
         String command = "curl 'https://www.muellmax.de/abfallkalender/evs/res/EvsStart.php' -H 'authority: www.muellmax.de' -H 'cache-control: max-age=0' -H 'origin: https://www.muellmax.de' -H 'upgrade-insecure-requests: 1' -H 'content-type: application/x-www-form-urlencoded' -H 'user-agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Mobile Safari/537.36' -H 'sec-fetch-user: ?1' -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' -H 'sec-fetch-site: same-origin' -H 'sec-fetch-mode: nested-navigate' -H 'referer: https://www.muellmax.de/abfallkalender/evs/res/EvsStart.php' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7' --data 'mm_ses=YWFRUStseWQ0blg5UnVCc1ZqenRCdklsQVovaGp3MTAwUWY2QmRWUUZ4Ky9NVU16S1pSbFM1eW9RNWdrT2RUdzRjcjNRcFluVWN2T0t4U0YvazJPbUZHTFM4VDlISTErVzBDODdORUQ0bGZVS3pObGs2enk0QmNTUGZaL0FzdXFNSElTY2JKdkNsRVVDajVZdjFTUnpNaDF5SHhoMDZTSVR0bjVBdDJIS0VvVFN1Uk5lMEJiaXVxSnl4cFJCMFpNN0FQOVJPQWFDdnNKT2ZwUThmcXVqN0VDTndjZklWdDd2aGFOWHNDTGxFSUlxZDFTUDJUem8xaTM1RjZvMHEwUk5nLzBiYi9hTVNrNVdKWm9nOHM3UU1JN2xPb2YwUExkclhOZ2w5YkdJeWc9&xxx=1&mm_frm_type=termine&mm_frm_fra_RM=RM&mm_frm_fra_BIO=BIO&mm_frm_fra_GS=GS&mm_ica_gen=iCalendar-Datei+laden' --compressed > " + new File(TrashCalenderPlugin.trashCalenderPlugin.getDataFolder(), "EVSTrash.ics").getAbsolutePath();
-        TaskOperation taskOperation = OperationRegister.getOperation("run_linux_shell");
+        ShellOperation shellOperation = new ShellOperation();
+        shellOperation.setUseSSH(false);
+        shellOperation.setScriptCommand(command);
+        shellOperation.setUseOutput(false);
+        addOperationData(shellOperation);
 
-        OperationSettings operationSettings = new OperationSettings();
-        operationSettings.addSetting("ssh.use", false);
-        operationSettings.addSetting("command.script", command);
-        operationSettings.addSetting("output.use", false);
-
-        addOperationData(taskOperation, operationSettings);
     }
 
     @Override
-    public void callback(Object object) {
-        OperationSettings operationSettings = (OperationSettings) object;
-        AppLogger.debug(Color.GREEN + "EVS calender pull finish " + operationSettings.getIntSetting("exit"));
+    public void callback(OperationOutput operationOutput) {
+
+        AppLogger.debug(Color.GREEN + "EVS calender pull finish " + operationOutput.getExit());
         TrashCalenderPlugin.trashCalenderPlugin.getTrashCalendar().loadCalendar();
     }
 
